b21146de7fdf6a663273878bf41969b129cce558
LucasClaes <lucas.claes@outlook.com>
2022-12-05 15:57:47 +0100

    Help iet ni werk

---
+++ SchoolProjecten/PythonDodona/Reeks2/  NieuweSchoolbanken.py
@@ -0,0 +1,4 @@
+LLA = int(input("Geef het aantal leerlingen in klas A:")) / 2
+LLB = int(input("Geef het aantal leerlingen in klas B:")) / 2
+LLC = int(input("Geef het aantal leerlingen in klas C:")) / 2
+print(int(LLA + LLB + LLC + 1))
