cd2cd3f7f3cc7e3bd3a627a4e61ead57e592b2b9
LucasClaes <lucas.claes@outlook.com>
2022-12-13 08:59:35 +0100

    Oplossing Even Of Oneven

---
+++ PythonDodona/Reeks3/EvenOfOneven.py
@@ -0,0 +1,7 @@
+getal = int(input("Geef een getal waarvan je wilt weten of het even of oneven is"))
+getal2 = getal // 2
+getal3 = getal / 2
+if getal3 > getal2:
+    print("EVEN")
+else:
+    print("ONEVEN")
