e17aa7db1489e3f74f85bf633a199c2cb9e2c61e
Lucas Claes <lucas.claes@outlook.com>
2022-12-13 19:03:30 +0100

    geen zin meer

--- PythonDodona/Reeks3/SchaakspelDonkerVeld.py
+++ PythonDodona/Reeks3/SchaakspelDonkerVeld.py
@@ -6,22 +6,31 @@ x1 = x_positie // 2
 x2 = x_positie / 2
 
 if x1 > x2:
-    x = "even"
-else:
     x = "oneven"
+else:
+    x = "even"
 
 y1 = y_positie // 2
 y2 = y_positie / 2
 
 if y1 > y2:
-    y = "even"
-else:
     y = "oneven"
-
-if x == "even" and y == "oneven":
-    print("LICHT VELD")
 else:
-    print("DONKER VELD")
+    y = "even"
+
+if x == "even":
+    if y == "even":
+        print("DONKER VELD")
+    else:
+        print("LICHT VELD")
+else:
+    if y == "oneven":
+        print("DONKER VELD")
+    else:
+        print("LICHT VELD")
+   
+
+    
 
 
 
