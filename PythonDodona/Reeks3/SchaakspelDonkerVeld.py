78236fcd72940095ac865ccc12c0895cf0c7dd9b
LucasClaes <lucas.claes@outlook.com>
2022-12-15 09:52:05 +0100

    Oplossing schaakseldonkerveld

--- PythonDodona/Reeks3/SchaakspelDonkerVeld.py
+++ PythonDodona/Reeks3/SchaakspelDonkerVeld.py
@@ -5,20 +5,25 @@ y_positie = int(input("Geef je y coordinaat: "))
 x1 = x_positie // 2
 x2 = x_positie / 2
 
-if x1 > x2:
-    x = "even"
-else:
+if x1 < x2:
     x = "oneven"
+else:
+    x = "even"
 
 y1 = y_positie // 2
 y2 = y_positie / 2
 
-if y1 > y2:
-    y = "even"
-else:
+if y1 < y2:
     y = "oneven"
+else:
+    y = "even"
+
+print(x,y)
+
 
 if x == "even" and y == "oneven":
+    print("LICHT VELD")
+if x == "oneven" and y == "even":
     print("LICHT VELD")
 else:
     print("DONKER VELD")
